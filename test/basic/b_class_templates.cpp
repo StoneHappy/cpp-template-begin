4f158b2d409560bb5b8f7cd06c9bdb6ac0d14b14
StoneHappy <18017158729@163.com>
2022-09-25 14:48:03 +0800

    🧪 test: friend mem func

--- test/basic/b_class_templates.cpp
+++ test/basic/b_class_templates.cpp
@@ -1,6 +1,12 @@
+#include "gtest/gtest.h"
+#include <exception>
 #include <gtest/gtest-spi.h>
 #include <gtest/gtest.h>
 
+#include <iostream>
+#include <ostream>
+#include <stdexcept>
+#include <utility>
 #include <vector>
 
 template<typename T>
@@ -16,6 +22,16 @@ public:
     bool empty() const {
         return m_elems.empty();
     }
+
+    void printOn(std::ostream& strm) const;
+
+    // friend
+    friend std::ostream& operator<< (std::ostream& strm, Stack<T> const& stack)
+    {
+        stack.printOn(strm);
+        return strm;
+    }
+
 private:
     std::vector<T> m_elems;
 };
@@ -40,14 +56,33 @@ T const& Stack<T>::top() const
     return m_elems.back();
 }
 
+template<typename T>
+void Stack<T>::printOn(std::ostream& strm) const {
+    for (T const& elem : m_elems) {
+        strm << elem << ' ';
+    }
+    strm << std::endl;
+}
+
 TEST(I_Basic, b_calss_templates)
 {
 	Stack<int> intStack;
     Stack<std::string> stringStack;
+    Stack<std::pair<int, int>> pairStack;
 
+    intStack.push(7);
+    intStack.push(8);
+    intStack.push(9);
     intStack.push(10);
     EXPECT_EQ(intStack.top(), 10);
 
-    stringStack.push("CPP");
-    EXPECT_EQ(stringStack.top(), "CPP");
+    stringStack.push("CPP1");
+    stringStack.push("CPP2");
+    stringStack.push("CPP3");
+    stringStack.push("CPP4");
+    EXPECT_EQ(stringStack.top(), "CPP4");
+
+    intStack.printOn(std::cout);
+    std::cout << stringStack;
+    // pairStack.printOn(std::cout); //Compile Error: std::pair does not define operator <<
 }
